131560dd4140b78433c67f2b746aa66aad23c66b
Andre <pandu_si1@outlook.com>
2025-05-08 02:51:04 +0800

    update

--- resources/views/livewire/status-lansia.blade.php
+++ resources/views/livewire/status-lansia.blade.php
@@ -12,7 +12,7 @@
     @if ($data->status == 'pending' && auth()->user()->hasRole('admin'))
         <h5 class="fw-semibold">Ubah Status</h5>
         <x-forms.select name="status" placeholder="Pilih Status" wire:model="status" select-type="normal">
-            <option value="" selected disabled>Pilih Status</option>
+            <option value="" selected>Pilih Status</option>
             <option value="success">Konfirmasi</option>
             <option value="reject">Tolak</option>
         </x-forms.select>
