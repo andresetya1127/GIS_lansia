bedf3062f1bb0ea2a205ccbb1fa5bda6f60fcb9b
Andre <pandu_si1@outlook.com>
2025-05-26 15:47:46 +0800

    Import

--- app/Imports/LansiaImport.php
+++ app/Imports/LansiaImport.php
@@ -5,6 +5,7 @@ namespace App\Imports;
 use App\Models\Lansia;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
 use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
@@ -27,43 +28,52 @@ class LansiaImport implements ToModel, WithStartRow, WithHeadingRow
      */
     public function model(array $row)
     {
+        try {
 
-        $data = [
-            'nama' => $row['nama_pm'],
-            'nik' => $row['nik_pm'],
-            'alamat' => $row['alamat'],
-            // 'lat'=> $row['lat'],
-            // 'lng'=> $row['lng'],
-            'tgl_lahir' => $row['tgl_lhr'],
-            'provinsi' => $row['provinsi'],
-            'kabupaten' => $row['kabupaten'],
-            'kecamatan' => $row['kecamatan'],
-            'desa' => $row['kelurahan'],
-            'rt' => $row['rt'],
-            'rw' => $row['rw'],
-            'user_id' => auth()->user()->id,
-            'status' => 'pending',
-        ];
+            $data = [
+                'nama' => $row['nama_pm'],
+                'nik' => $row['nik_pm'],
+                'alamat' => $row['alamat'],
+                // 'lat'=> $row['lat'],
+                // 'lng'=> $row['lng'],
+                'tgl_lahir' => $row['tgl_lhr'],
+                'provinsi' => $row['provinsi'],
+                'kabupaten' => $row['kabupaten'],
+                'kecamatan' => $row['kecamatan'],
+                'desa' => $row['kelurahan'],
+                'rt' => $row['rt'],
+                'rw' => $row['rw'],
+                'user_id' => auth()->user()->id,
+                'status' => 'pending',
+            ];
 
-        $location = $this->cordinates($row['kelurahan'].','.$row['kecamatan']);
+            $validator = Validator::make($data, [
+                'nik' => ['unique:lansias,nik'],
+            ]);
 
-        if (!empty($location)) {
-            $data['lat'] = $location['lat'];
-            $data['lng'] = $location['lon'];
+            if (!$validator->fails()) {
+                $location = $this->cordinates($row['kelurahan'] . ',' . $row['kecamatan']);
+
+                if (!empty($location)) {
+                    $data['lat'] = $location['lat'];
+                    $data['lng'] = $location['lon'];
+                }
+
+                return new Lansia($data);
+            }
+        } catch (\Throwable $th) {
+            dd($th);
         }
-
-        return new Lansia($data);
     }
 
     public function cordinates($query)
     {
         try {
             $json = Http::withHeaders(['User-Agent' => 'WebGis/1.0'])
-                ->get('https://nominatim.openstreetmap.org/search?addressdetails=1&q='.$query.'&format=jsonv2&limit=1');
+                ->get('https://nominatim.openstreetmap.org/search?addressdetails=1&q=' . $query . '&format=jsonv2&limit=1');
             return $json->json()[0];
         } catch (\Throwable $th) {
             return [];
         }
     }
-
 }
